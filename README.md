02c493e7ded18ec0a5f9327a3456037683335360
mayuan <mayuanucas@qq.com>
2019-03-20 10:46:20 +0800

    acfg2.0

--- README.md
+++ README.md
@@ -12,31 +12,23 @@ pip3 install -r requirements.txt
 ```bash
 python3 main.py -h
 
-usage: main.py [-h] [-v] [-t] [-o OUTPUT] inputFile
+usage: main.py [-h] [-v] [-o OUTPUT] inputFile
 
 提取二进制程序特征
 
 positional arguments:
-  inputFile             二进制文件或路径列表文件
+  inputFile             二进制文件
 
 optional arguments:
   -h, --help            show this help message and exit
   -v, --version         show program's version number and exit
-  -t, --text            从文本中读取二进制程序路径
   -o OUTPUT, --output OUTPUT
-                        =存储路径
+                        =存储目录
 ```
 
 ## Example
 
 ### From single binary
 ```bash
-python3 main.py -o result.txt data/busybox
-```
-
-### From massive binary
-```bash
-# The result is stored in the database by default.
-# Each line in the text "input.txt" represents the path of a binary file.
-python3 main.py -t input.txt
+python3 main.py -o ./out/ data/busybox
 ```
--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# -*- coding:utf-8 -*-
 import angr
 import argparse
 import os
-from angrutils import *
-import multiprocessing as mp
-import pymongo
 import json
-
-# 数据库链接URL
-DBURL = "mongodb://10.10.2.192:27017/"
-# 数据库名
-DB = "feature"
-# 集合名
-COL = "bin"
+from angrutils import *
 
 
 def in_ins_list(instruction, ins_list):
@@ -77,7 +68,7 @@ def handle_block(block, no_str):
 
 def handle_function(entry_func, bin_path, output_path=None):
     """
-    提取每个函数的特征并存储
+    提取每个函数的特征并存储到文本
     :param entry_func:
     :param bin_path:
     :param output_path:
@@ -88,88 +79,73 @@ def handle_function(entry_func, bin_path, output_path=None):
     function_feature["function_name"] = entry_func.name
     function_feature["features"] = []
     function_feature["adj"] = []
-    # no of string constants
-    f_no_string = len(entry_func.string_references())
-    block_cnt = 0
-    # 提取函数内每个基本块的属性
-    for blk in entry_func.blocks:
-        function_feature["features"].append(handle_block(blk, f_no_string))
-        block_cnt = block_cnt + 1
-    function_feature["block"] = block_cnt
-    # 节点数目为0时直接返回
-    if 0 == len(entry_func.graph):
-        return
-    # 节点的邻接矩阵
-    matrix = nx.adjacency_matrix(entry_func.graph).todense().tolist()
-    for i, line in enumerate(matrix):
-        # 当前节点到自己无边
-        line[i] = 0
-        no_offspring = line.count(1)
-        function_feature["features"][i][-1] = no_offspring
-        function_feature["adj"].append(line)
-    if output_path is None:
-        return function_feature
-    else:
-        with open(output_path, "a+", encoding="utf-8") as f:
-            f.writelines(json.JSONEncoder().encode(function_feature) + '\n')
+    try:
+        # no of string constants
+        f_no_string = len(entry_func.string_references())
+        block_cnt = 0
+        # 提取函数内每个基本块的属性
+        for blk in entry_func.blocks:
+            function_feature["features"].append(handle_block(blk, f_no_string))
+            block_cnt = block_cnt + 1
+        function_feature["block"] = block_cnt
+        # 节点数目为0时直接返回
+        if 0 == len(entry_func.graph):
+            return
+        # 节点的邻接矩阵
+        matrix = nx.adjacency_matrix(entry_func.graph).todense().tolist()
+        for i, line in enumerate(matrix):
+            # 当前节点到自己无边
+            line[i] = 0
+            no_offspring = line.count(1)
+            function_feature["features"][i][-1] = no_offspring
+            function_feature["adj"].append(line)
+        if output_path is None:
+            print(function_feature)
+        else:
+            output_file = output_path + os.path.sep + function_feature["function_name"] + ".json"
+            with open(output_file, "w", encoding="utf-8") as f:
+                f.writelines(json.JSONEncoder().encode(function_feature))
+    except Exception as e:
+        print("Exception->", e)
 
 
 def handle_bin(bin_file, output_path=None):
+    """
+    逆向二进制文件,以函数为基本单位进行划分,提取函数特征
+    :param bin_file:
+    :param output_path:
+    :return:
+    """
     if not os.path.isfile(bin_file):
         print("文件不存在->", bin_file)
         return
-    db = None
-    if output_path is None:
-        client = pymongo.MongoClient(DBURL)
-        db = client[DB]
+    if output_path is not None:
+        if not os.path.isdir(output_path):
+            os.makedirs(output_path)
     try:
         proj = angr.Project(bin_file, auto_load_libs=False)
         # cfg = proj.analyses.CFGEmulated()
         cfg = proj.analyses.CFGFast()
         for func in cfg.kb.functions.values():
-            if output_path is not None:
-                handle_function(cfg.kb.functions[func.addr], bin_file.strip(), output_path)
-            else:
-                feature = handle_function(cfg.kb.functions[func.addr], bin_file.strip())
-                if feature is not None:
-                    db[COL].insert(feature)
+            handle_function(cfg.kb.functions[func.addr], bin_file.strip(), output_path)
     except Exception as e:
-        print("Exception->", bin_file)
-
-
-def main_text(text):
-    """
-    从文本中逐行读取二进制程序的路径并进行处理
-    :param text:
-    :return:
-    """
-    if not os.path.isfile(text):
-        print("文件不存在->", text)
-        return
-    pool = mp.Pool(processes=8)
-    with open(text, "r") as f:
-        for line in f:
-            print("正在提取->", line)
-            pool.apply_async(handle_bin, args=(line.strip(),))
-        pool.close()
-        pool.join()
+        print("Exception->", bin_file, e)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="提取二进制程序特征")
-    parser.add_argument('-v', '--version', action='version', version='acfg 1.0')
-    parser.add_argument('-t', '--text', action='store_true', help='从文本中读取二进制程序路径')
-    parser.add_argument('-o', '--output', type=str, default=None, help='=存储路径')
-    parser.add_argument('inputFile', help='二进制文件或路径列表文件')
+    parser.add_argument('-v', '--version', action='version', version='acfg 2.0')
+    parser.add_argument('-o', '--output', type=str, default=None, help='=存储目录')
+    parser.add_argument('inputFile', help='二进制文件')
     args = parser.parse_args()
 
-    # 输入为保存所有二进制程序路径的列表文件
-    if args.text:
-        main_text(args.inputFile)
-        print("all task done!")
+    if not os.path.isfile(args.inputFile):
+        print("输入不是二进制文件!")
     else:
         if args.output is not None:
+            # 将函数特征存储到文本中
             handle_bin(args.inputFile, args.output)
             print("done->", args.output)
         else:
-            print("请指定输出文件路径!")
+            # 直接输出每个函数的特征
+            handle_bin(args.inputFile)
